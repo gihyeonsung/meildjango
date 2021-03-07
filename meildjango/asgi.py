8ef6fba32a99f5b8ace9e28b032054327e9db30b
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-03-07 13:30:46 +0900

    initial commit

---
+++ meildjango/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for meildjango project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meildjango.settings')
+
+application = get_asgi_application()
