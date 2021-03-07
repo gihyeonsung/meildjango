326a9bd0f9585ba2e77eb49c3afa00a01de0e58a
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-03-07 13:47:22 +0900

    feat: create log and habit models

--- habit/models.py
+++ habit/models.py
@@ -1,2 +1,17 @@
 from django.db import models
 
+
+class Log(models.Model):
+    memo = models.CharField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Habit(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    duration = models.TimeField()
+    count = models.IntegerField()
+    logs = models.ForeignKey(Log, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
