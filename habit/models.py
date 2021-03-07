a83d02dbdb6df71d26d2fdaf9da878902f1fe035
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-03-07 13:51:28 +0900

    refactor: use proper field and attrs on models

--- habit/models.py
+++ habit/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class Log(models.Model):
-    memo = models.CharField()
+    memo = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
 
 class Habit(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     description = models.TextField()
-    duration = models.TimeField()
-    count = models.IntegerField()
+    duration = models.DurationField()
+    count = models.PositiveIntegerField()
     logs = models.ForeignKey(Log, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
