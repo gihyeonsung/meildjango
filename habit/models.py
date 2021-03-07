a7e561774f8e855ed32a7fa0608d7cd9252c1e2d
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-03-07 18:57:06 +0900

    fix: move foreignkey field to log

--- habit/models.py
+++ habit/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 
-class Log(models.Model):
-    memo = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-
 class Habit(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     duration = models.DurationField()
     count = models.PositiveIntegerField()
-    logs = models.ForeignKey(Log, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Log(models.Model):
+    memo = models.TextField()
+    logs = models.ForeignKey(Habit, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
