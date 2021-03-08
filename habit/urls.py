e9c6be3abeea8b7ac35b0a832850bc1e8bcb673a
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-03-08 22:26:28 +0900

    refactor: replace hardcoded with url tags

--- habit/urls.py
+++ habit/urls.py
@@ -7,6 +7,6 @@ app_name = 'habit'
 urlpatterns = [
     path('', HabitListView.as_view()),
     path('<int:pk>/', HabitDetailView.as_view(), name='habitdetail'),
-    path('create', HabitCreateView.as_view()),
-    path('<int:pk>/logs/create', LogCreateView.as_view()),
+    path('create', HabitCreateView.as_view(), name='habitcreate'),
+    path('<int:pk>/logs/create', LogCreateView.as_view(), name='logcreate'),
 ]
