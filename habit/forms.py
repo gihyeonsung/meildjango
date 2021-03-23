c554e095318e8188cdc6608398523a83f735a4cb
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-03-23 19:26:47 +0900

    feat: add habitform labels (#14)

--- habit/forms.py
+++ habit/forms.py
@@ -7,6 +7,12 @@ class HabitForm(forms.ModelForm):
     class Meta:
         model = Habit
         fields = ['title', 'description', 'duration', 'count']
+        labels = {
+            'title': '만들 습관의 이름이 뭔가요?',
+            'description': '어떤 습관인가요?',
+            'duration': '얼마나 자주 반복하나요?',
+            'count': '얼마나 많이 반복하나요?',
+        }
 
 
 class LogForm(forms.ModelForm):
