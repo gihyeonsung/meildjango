fea28a5c7b3c0261b48c461dd5f9493cbb227620
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-03-28 15:41:41 +0900

    feat: use daysdurationfield (#5)

--- habit/forms.py
+++ habit/forms.py
@@ -1,16 +1,26 @@
 from django import forms
+from django.utils import timezone
 
 from .models import Habit, Log
 
 
+class DaysDurationField(forms.IntegerField):
+
+    def to_python(self, value):
+        days = super().to_python(value)
+        return days * timezone.timedelta(days=1)
+
+
 class HabitForm(forms.ModelForm):
+
+    duration = DaysDurationField(label='몇일마다 반복하나요?', initial=1)
+
     class Meta:
         model = Habit
         fields = ['name', 'description', 'duration', 'count']
         labels = {
             'name': '만들 습관의 이름이 뭔가요?',
             'description': '어떤 습관인가요?',
-            'duration': '얼마나 자주 반복하나요?',
             'count': '얼마나 많이 반복하나요?',
         }
 
